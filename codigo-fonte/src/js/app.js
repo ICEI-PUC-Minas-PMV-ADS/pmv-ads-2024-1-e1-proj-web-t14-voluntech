1e69a7e368c17c12e246a3625cc50b907f759e8d
João Victor <joao.nery@sga.pucminas.br>
2024-05-19 15:29:29 -0300

    Archives changes

--- codigo-fonte/src/js/app.js
+++ codigo-fonte/src/js/app.js
@@ -85,3 +85,20 @@ cep.addEventListener('input', assync => {
       cidade.value = data.localidade + ' / ' + data.uf;
     });
 })
+
+// Botão com efeito para senha
+function password_show_hide() {
+  var x = document.getElementById("password");
+  var show_eye = document.getElementById("show_eye");
+  var hide_eye = document.getElementById("hide_eye");
+  hide_eye.classList.remove("d-none");
+  if (x.type === "password") {
+    x.type = "text";
+    show_eye.style.display = "none";
+    hide_eye.style.display = "block";
+  } else {
+    x.type = "password";
+    show_eye.style.display = "block";
+    hide_eye.style.display = "none";
+  }
+}
