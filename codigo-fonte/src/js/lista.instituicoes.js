f593663573f2d52dcc5afd231aa41c743a85b3b4
lis <lismlm29@gmail.com>
2024-06-02 23:43:53 -0300

    filtros

--- codigo-fonte/src/js/lista.instituicoes.js
+++ codigo-fonte/src/js/lista.instituicoes.js
@@ -1,41 +1,57 @@
 let cardONG =
 [
-    {
-      "nomeInstituicao": "Anjinhos Protetores",
-      "cep": "32315-170",
-      "cnpj": "13.475.865/0001-56",
-      "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
-      "categorias": "saude",
-      "cidade": "Contagem",
-      "email": "fredkarklin@gmail.com",
-      "telefone": "(31) 99477-0070",
-      "password": "Teste123",
-      "celular": "(31) 99999-9999"
-    },
-    {
-      "nomeInstituicao": "Anjinhos Protetores",
-      "cep": "32315-170",
-      "cnpj": "13.475.865/0001-56",
-      "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
-      "categorias": "saude",
-      "cidade": "Contagem",
-      "email": "fredkarklin@gmail.com",
-      "telefone": "(31) 99477-0070",
-      "password": "Teste123",
-      "celular": "(31) 99999-9999"
-    },
-    {
-      "nomeInstituicao": "Anjinhos Protetores",
-      "cep": "32315-170",
-      "cnpj": "13.475.865/0001-56",
-      "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
-      "categorias": "saude",
-      "cidade": "Contagem",
-      "email": "fredkarklin@gmail.com",
-      "telefone": "(31) 99477-0070",
-      "password": "Teste123",
-      "celular": "(31) 99999-9999"
-    }
-  ]
+  {
+    "nomeInstituicao": "Teste Saude Contagem Minas Gerais",
+    "cep": "32315-170",
+    "cnpj": "13.475.865/0001-56",
+    "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
+    "categorias": "Saúde",
+    "cidade": "Contagem",
+    "estado": "Minas Gerais",
+    "telefone": "(31) 99477-0070",
+    "password": "Teste123",
+    "celular": "(31) 99999-9999",
+    "foto": src="img/Anjinhos Protetores - Resgate e abrigo de animais.jpg"
+  },
+  {
+    "nomeInstituicao": "Teste Outras Joinville Minas Gerais",
+    "cep": "32315-170",
+    "cnpj": "13.475.865/0001-56",
+    "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
+    "categorias": "Outras",
+    "cidade": "Joinville",
+    "estado": "Minas Gerais",
+    "telefone": "(31) 99477-0070",
+    "password": "Teste123",
+    "celular": "(31) 99999-9999",
+    "foto": src="img/Anjinhos Protetores - Resgate e abrigo de animais.jpg"
+  },
+  {
+    "nomeInstituicao": "Teste Educação Contagem Minas Gerais",
+    "cep": "32315-170",
+    "cnpj": "13.475.865/0001-56",
+    "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
+    "categorias": "Educação",
+    "cidade": "Contagem",
+    "estado": "Minas Gerais",
+    "telefone": "(31) 99477-0070",
+    "password": "Teste123",
+    "celular": "(31) 99999-9999",
+    "foto": src="img/Anjinhos Protetores - Resgate e abrigo de animais.jpg"
+  },
+  {
+    "nomeInstituicao": "Teste Saúde Fortaleza Ceará",
+    "cep": "32315-170",
+    "cnpj": "13.475.865/0001-56",
+    "bairro": "Rua José Olinto Fontes - Cidade Jardim Eldorado",
+    "categorias": "Saúde",
+    "cidade": "Fortaleza",
+    "estado": "Ceara",
+    "telefone": "(31) 99477-0070",
+    "password": "Teste123",
+    "celular": "(31) 99999-9999",
+    "foto": src="img/Anjinhos Protetores - Resgate e abrigo de animais.jpg"
+  }
+]
 
   localStorage.setItem("cardONG", JSON.stringify(cardONG));
