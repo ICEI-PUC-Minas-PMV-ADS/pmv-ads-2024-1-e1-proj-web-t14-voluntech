8dfc8bc093c4a7f4802dcefe300fbd6cbd551823
Lucas <lucas.costa.1534195@sga.pucminas.br>
2024-06-02 21:20:02 -0300

    Persistir dados Atualização de cadastro

--- codigo-fonte/src/js/atualizar.js
+++ codigo-fonte/src/js/atualizar.js
@@ -219,3 +219,52 @@
       length.classList.add("invalid");
     }
   };
+
+  // persistir os dados do formulário
+     
+  function salvar() {
+     
+    localStorage.setItem("nomeInstituicao" , document.querySelector("input#nomeInstituicao").value)
+    
+    localStorage.setItem("cep" , document.querySelector("input#cep").value)
+    
+    localStorage.setItem("cnpj" , document.querySelector("input#cnpj").value)
+    
+    localStorage.setItem("bairro" , document.querySelector("input#bairro").value)
+    
+    
+    localStorage.setItem("cidade" , document.querySelector("input#cidade").value)
+    
+    localStorage.setItem("email" , document.querySelector("input#email").value)
+    
+    localStorage.setItem("telefone" , document.querySelector("input#telefone").value)
+    
+    localStorage.setItem("password" , document.querySelector("input#password").value)
+    
+    localStorage.setItem("celular" , document.querySelector("input#celular").value)
+
+  }
+
+  {
+
+  document.querySelector("input#nomeInstituicao").value = localStorage.getItem("nomeInstituicao");
+  
+  document.querySelector("#cep").value = localStorage.getItem("cep");
+
+  document.querySelector("input#cnpj").value = localStorage.getItem("cnpj");
+ 
+  document.querySelector("#bairro").value = localStorage.getItem("bairro");
+
+
+
+  document.querySelector("#cidade").value = localStorage.getItem("cidade");
+
+  document.querySelector("#email").value = localStorage.getItem("email").value;
+
+  document.querySelector("#telefone").value = localStorage.getItem("telefone");
+
+  document.querySelector("#password").value = localStorage.getItem("password");
+
+  document.querySelector("#celular").value = localStorage.getItem("celular");
+
+  }
