005b954e82c1c0e9eec8e600c1107daa44b2a6c5
João Victor <joao.nery@sga.pucminas.br>
2024-06-07 20:53:14 -0300

    Updates atualizar,js

--- codigo-fonte/src/js/atualizar.js
+++ codigo-fonte/src/js/atualizar.js
@@ -29,27 +29,36 @@ document.querySelector("input#email").value = usuarioLogado.email;
 
 document.querySelector("select#categorias").value = usuarioLogado.categorias;
 
-// Função para atualizar os dados
+// Função para atualizar
 function atualizar() {
 
-  localStorage.setItem("nomeInstituicao", document.querySelector("input#nomeInstituicao").value)
+  const nomeInstituicao = localStorage.setItem("nomeInstituicaoLogada", document.querySelector("input#nomeInstituicao").value);
+  const cep = localStorage.setItem("cep", document.querySelector("input#cep").value);
+  const descricao = localStorage.setItem("descricao", document.querySelector("input#descricao").value);
+  const rua = localStorage.setItem("rua", document.querySelector("input#rua").value);
+  const cnpj = localStorage.setItem("cnpj", document.querySelector("input#cnpj").value);
+  const bairro = localStorage.setItem("bairro", document.querySelector("input#bairro").value);
+  const cidade = localStorage.setItem("cidade", document.querySelector("input#cidade").value);
+  const telefone = localStorage.setItem("telefone", document.querySelector("input#telefone").value);
+  const password = localStorage.setItem("password", document.querySelector("input#password").value);
+  const celular = localStorage.setItem("celular", document.querySelector("input#celular").value);
+  const email = localStorage.setItem("email", document.querySelector("input#email").value);
+  const categorias = localStorage.setItem("categorias", document.querySelector("select#categorias").value);
 
-  localStorage.setItem("cep", document.querySelector("input#cep").value)
-
-  localStorage.setItem("cnpj", document.querySelector("input#cnpj").value)
-
-  localStorage.setItem("bairro", document.querySelector("input#bairro").value)
-
-  localStorage.setItem("cidade", document.querySelector("input#cidade").value)
-
-  localStorage.setItem("telefone", document.querySelector("input#telefone").value)
-
-  localStorage.setItem("password", document.querySelector("input#password").value)
-
-  localStorage.setItem("celular", document.querySelector("input#celular").value)
-
-  localStorage.setItem("email", document.querySelector("input#email").value)
-
-  localStorage.setItem("categorias", document.querySelector("input#categorias").value)
-
+  // Exibir mensagem de sucesso
+  Swal.fire({
+    title: 'Sucesso!',
+    text: 'Atualização efetuada com sucesso.',
+    icon: 'success',
+    confirmButtonText: 'OK',
+    customClass: {
+      confirmButton: "btn btn-success",
+    },
+    buttonsStyling: false
+  }).then(() => {
+    setTimeout(() => {
+      window.location.href = "feed.html"; // Substitua pelo URL da sua página de feed
+    }
+      , 2000);
+  });
 }
