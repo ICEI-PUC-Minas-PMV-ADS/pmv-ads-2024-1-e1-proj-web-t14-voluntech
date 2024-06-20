49419b0e1af147f109be0e98a94fba42a5f587ee
Lucas <lucas.costa.1534195@sga.pucminas.br>
2024-06-19 22:08:31 -0300

    restaurado sem codigo para avaliações

--- codigo-fonte/src/js/instituicoes.js
+++ codigo-fonte/src/js/instituicoes.js
@@ -132,18 +132,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
     gerarCards();
 });
-
-
-//captar avaliações em forma de estrelas
-var stars = document.querySelectorAll('.star-icon');
-
-document.addEventListener('click', function(e){
-    var classStar = e.target.classList;
-    if(!classStar.contains('ativo')){
-         stars.forEach(function(star){
-             star.classList.remove('ativo');
-         });
-          classStar.add('ativo');
-          console.log(e.target.getAttribute('data-avaliacao'));
-        }
-});
